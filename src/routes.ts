d89f461b99910a841fdc36cf75511bc130fa8043
albertoparente <albertoparentefh@gmail.com>
2022-05-04 00:09:27 -0300

    Adding router get(users) and crating method GetAllUserController

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,16 @@
 import { Router, Request, Response } from 'express';
 import { CreateUsersController } from './controllers/CreateUsersControlle';
+import { GetAllUserController } from './controllers/GetAllUserController';
 
 const router = Router();
 const createUserController = new CreateUsersController();
+const getAllUserController = new GetAllUserController();
 
 router.get('/', (request: Request, response: Response) => {
     return response.json({ message: 'Test...' })
 });
 
 router.post('/users', createUserController.handle);
+router.get('/users', getAllUserController.handle);
 
 export { router }
