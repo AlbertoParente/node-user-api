e51329167949640b2e6abf1101840bb6d82c46fc
albertoparente <albertoparentefh@gmail.com>
2022-05-05 22:00:23 -0300

    Creating DeleteUserController using the TDD pattern

---
+++ src/services/DeleteUserService.test.ts
@@ -0,0 +1,26 @@
+import createConnection from '../database';
+import { getConnection } from 'typeorm';
+import { FakeData } from '../utils/FakeData';
+import { DeleteUserService } from './DeleteUserService'
+
+describe('DeleteUserService', () => {
+    beforeAll(async () => {
+        const connection = await createConnection();
+        await connection.runMigrations();
+    });
+
+    afterAll(async () => {
+        const connection = getConnection();
+        await connection.close();
+    });
+
+    const fakeData = new FakeData();
+
+    it('should return a empity array when the user is deleted!', () => {
+        const mockeUser = await fakeData();
+        const deleteUserService = new DeleteUserService();
+        const result = await deleteUserService.execute({ id });
+
+        expect(result).toHaveLength(0);
+    });
+});
